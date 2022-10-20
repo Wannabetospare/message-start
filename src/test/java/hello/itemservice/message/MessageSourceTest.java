06800b32fb487eaaa1adbecc23cd8e45d27ef697
Wannabetospare <qaz4430@naver.com>
2022-10-20 20:19:51 +0900

    주석복습까지

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,4 +1,6 @@
 package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +11,11 @@ import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.*;
 
-
-
 @SpringBootTest
 public class MessageSourceTest {
 
-
     @Autowired
     MessageSource ms;
-
 
     @Test
     void helloMessage() {
@@ -25,35 +23,38 @@ public class MessageSourceTest {
         assertThat(result).isEqualTo("안녕");
     }
 
+    // 메시지가 없을 경우 발생하는 오류
     @Test
     void notFoundMessageCode() {
         assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
                 .isInstanceOf(NoSuchMessageException.class);
     }
 
+    // 메시지가 없어도 defaultMessage 를 사용하면 오류가 발생하지 않는다.
     @Test
     void notFoundMessageCodeDefaultMessage() {
         String result = ms.getMessage("no_code", null, "기본 메시지", null);
         assertThat(result).isEqualTo("기본 메시지");
     }
 
+    // 리스트형도 가능
     @Test
     void argumentMessage() {
-        String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
-        assertThat(result).isEqualTo("안녕 Spring");
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
     }
 
+    // locale 값이 없는거나 한국어나 똑같아서 테스트 통과
     @Test
     void defaultLang() {
         assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
         assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
 
+
+    // locale 이 영어라서 영어버전의 hello 가 나와야 한다.
     @Test
     void enLang() {
         assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
     }
-
-
-
 }
